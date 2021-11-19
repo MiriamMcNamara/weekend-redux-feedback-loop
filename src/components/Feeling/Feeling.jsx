76a73a53dbc2af533f2813d8081eceb22d3d3af8
Miriam McNamara <miriammcnamara@miriamscomputer.lan>
2021-11-19 14:11:07 -0600

    redux and store setup

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -1,15 +1,23 @@
 import { Grid, Typography, Box, Card, TextField, Button } from "@material-ui/core";
 import { useState } from "react";
+import {useDispatch, useSelector} from 'react-redux';
 
 function Feeling( props ){
     // const[ name, setName ]=useState( null );
+    const dispatch = useDispatch();
 
-    const handleCloseAgree = () => {
+    const[ feelingInput, setFeelingInput ]=useState( 0 );
+
+    const handleInput = ()=>{ //input capture
+        console.log( 'in handleInput:', event.target.value);
+        setFeelingInput( event.target.value );
+    }
+
+    const handleCloseAgree = () => { //button click handler
         console.log('in handleCloseAgree' );
-        //capture the input for the textfield and store in variable
-        //if (textfield = 1-5) { link to next page}
-        //if (textfield = empty ) { pop up dialogue}
-        
+        //if (textfield = 1-5) { send data to the store and link to next page}
+        //if (textfield = 0 ) { pop up dialogue, bc it isn't a num you can select}
+        //dispatch( { type: 'ADD_CUSTOMER',  payload: { newCustomer } } )
     }
 
     return(
@@ -21,7 +29,7 @@ function Feeling( props ){
                 <Card sx={{ maxWidth: 400 }}>
                     <Typography variant="h4" align="center" color="secondary">1 of 4 Pages</Typography>
                     <Typography align="center">How are you feeling today?</Typography>
-                    <TextField type="number" inputProps={{ min: "1", max: "5" }}></TextField>
+                    <TextField type="number" inputProps={{ min: "1", max: "5" }} onChange={(event ) =>handleInput ( event )}></TextField>
                     <Button variant="contained" size="large" color="secondary" onClick={handleCloseAgree}>NEXT</Button>
                 </Card>
             </Grid>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//reducers go here
+const feedback = ( state=[], action)=>{
+    if( action.type === 'ADD_FEEDBACK'){
+      return[...state, action.payload];
+    }
+    return state;
+  }
+
+const storeInstance = createStore(
+    combineReducers(
+      {
+        feedback
+      }
+    ),
+    applyMiddleware(
+      logger
+    )
+  );
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
